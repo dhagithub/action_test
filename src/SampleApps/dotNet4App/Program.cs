702e54c232e3efd69a9a4aed3dc04a1d805c5dba
日向弘一 <hirokazeon@gmail.com>
2023-10-26 22:36:27 +0900

    最初のコミット

---
+++ src/SampleApps/dotNet4App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet4App
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine(".NET Framework 4.8");
+			Console.ReadKey();
+        }
+	}
+}
---
+++ src/SampleApps/dotNet6App/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace dotNet6App
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(".NET 6");
+			Console.ReadKey();
+		}
+	}
+}
